fddf9d2a72d5d532adf373d6208a981a1f6a5b59
Jeroen Masschelein <jeroen.masschelein@gmail,com>
2017-10-03 17:31:29 -0400

    FB model

---
+++ db/migrate/20171003213010_add_omniauth_to_users.rb
@@ -0,0 +1,9 @@
+class AddOmniauthToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :oauth_tokens, :string
+    add_column :users, :oauth_expires_at, :datetime
+  end
+end
